859acf8ee7472236662049322e75d1e30f245d4c
HYUNJUN, KIM <denny10002@naver.com>
2023-02-22 23:52:37 +0900

    Chapter 7 Study

--- src/main/java/hello/itemservice/domain/item/ItemRepository.java
+++ src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -2,7 +2,9 @@ package hello.itemservice.domain.item;
 
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Repository
@@ -17,5 +19,20 @@ public class ItemRepository {
         item.setId(++sequence);
         store.put(item.getId(), item);
         return item;
+    }
+
+    public Item findById(Long id) {
+        return store.get(id);
+    }
+
+    public List<Item> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void update(Long itemId, Item updateParam) {
+        Item findItem = findById(itemId);
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
     }
 }
